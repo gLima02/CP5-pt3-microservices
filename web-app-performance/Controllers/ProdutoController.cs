0bb9714693b70603bb014fd8a943323dde98612e
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-01 20:35:31 -0300

    finalizado Checkpoint 05 - RM93401

---
+++ web-app-performance/Controllers/ProdutoController.cs
@@ -0,0 +1,106 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using web_app_performance.Model;
+
+namespace web_app_performance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private static ConnectionMultiplexer redis;
+
+        [HttpGet]
+        public async Task<IActionResult> GetProduto()
+        {
+            string key = "getproduto";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyExpireAsync(key, TimeSpan.FromMinutes(10));
+            string user = await db.StringGetAsync(key);
+
+            if (!string.IsNullOrEmpty(user))
+            {
+                return Ok(user);
+            }
+
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123;";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+            string query = "select Id, nome, preco, quantidade_estoque, data_criacao from produtos;";
+            var produtos = await connection.QueryAsync<Produto>(query);
+            string produtosJson = JsonConvert.SerializeObject(produtos);
+            await db.StringSetAsync(key, produtosJson);
+
+            return Ok(produtos);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Produto produto)
+        {
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123;";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            string sql = @"INSERT INTO produtos(nome, preco, quantidade_estoque, data_criacao) 
+               VALUES (@nome, @preco, @quantidade_estoque, NOW());";
+            await connection.ExecuteAsync(sql, produto);
+
+            //apagar o cachê
+            string key = "getproduto";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] Produto produto)
+        {
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123;";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            string sql = @"update produtos 
+                            set nome = @nome,
+                                preco = @preco,
+                                quantidade_estoque = @quantidade_estoque,
+                                data_criacao = @data_criacao
+                            where Id = @id;";
+
+            await connection.ExecuteAsync(sql, produto);
+
+            //apagar o cachê
+            string key = "getproduto";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123;";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            string sql = @"delete from produtos where Id = @id;";
+
+            await connection.ExecuteAsync(sql, new { id });
+
+            //apagar o cachê
+            string key = "getproduto";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+
+            return Ok();
+        }
+    }
+}
